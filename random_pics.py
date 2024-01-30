29fa718132a1f09b558c81fa586e151ddc3baa8f
spacehur <kzulpanov@dengabank.ru>
2024-01-30 10:29:54 +0300

    ticket-1 [add] deletion after sending pic

--- random_pics.py
+++ random_pics.py
@@ -1,3 +1,5 @@
+import os
+
 from birth.birthday_check import BirthdayReminder
 from config import RANDOM_PIC_PATH
 from main import send_photo
@@ -6,6 +8,7 @@ from main import send_photo
 def send_random_pic():
     pic = BirthdayReminder().get_random_file(RANDOM_PIC_PATH)
     send_photo(photo=pic, text=f"Случайное фото {pic.split('random_pics/')[-1]}")
+    os.remove(pic)
 
 
 if __name__ == '__main__':
